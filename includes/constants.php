a14c6490c69c18dd5946ea41520614b60b0f4f37
jatinsumai50 <43413309+jatinsumai50@users.noreply.github.com>
2019-02-03 16:03:17 +0530

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,6 @@
 <?php 
 
-define("BASEURL", "http://localhost/blog/");
+define("BASEURL", "http://localhost/cms/");
 define("POSTIMAGES", BASEURL."images/");
 define("ADMINIMAGES", BASEURL."admin/images/");
 define("LAYOUT", BASEURL."includes/layouts/");
