2e6e7628ccc1fec38b3f68e017fe8eff261d3664
Jatin Sumai <43413309+jatinsumai50@users.noreply.github.com>
2019-02-13 21:16:29 +0530

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -8,12 +8,4 @@ define("LAYOUT", BASEURL."includes/layouts/");
 define("MAX_PAGE_CONTENT", 5);
 ?>
 
-<?php 
-
-define("BASEURL", "http://localhost/cms/");
-define("POSTIMAGES", BASEURL."images/");
-define("ADMINIMAGES", BASEURL."admin/images/");
-define("LAYOUT", BASEURL."includes/layouts/");
-
-?>
 
